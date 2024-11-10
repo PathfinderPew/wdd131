28555c79bc531e2af038873e1b8c7a7448e0a796
Brandon P <pew16002@byui.edu>
2024-11-10 00:04:54 -0700

    Initial setup for WDD 131 project

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display last modified date
+document.getElementById("lastModified").textContent = "Last Updated: " + document.lastModified;
